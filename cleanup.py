8106aac2be079cbb3b0a4ec844415eba64858961
AkersJack <jackakers@umbc.edu>
2023-12-07 22:41:19 -0500

    some files

---
+++ cleanup.py
@@ -0,0 +1,393 @@
+import json 
+import csv 
+import re
+import time 
+from PIL import Image
+import pickle 
+import os
+import numpy as np
+import pandas as pd
+import base64
+import shutil
+from sklearn.impute import KNNImputer
+
+def check_images(): 
+    pictures = os.listdir("D:/weather")
+    counter = 0
+    for picture in pictures:
+        if counter % 100 == 0: 
+            print(f"{counter}/{len(pictures)}")
+        image_path = os.path.join("D:/weather/", picture)
+        image = Image.open(image_path)
+        width, height = image.size
+        # print(f"Width: {width}, Height: {height}")
+        if width != 760 or height != 616: 
+            print(f"Bad size: {picture} ")
+            time.sleep(10)
+        counter += 1
+
+def parsetext(text):
+    regex = r"^(.*?)\|(.*?)\|(.*?)$"
+    match = re.match(regex, text)
+    if match: 
+        amount = match.group(1)
+        location = match.group(2)
+        time = match.group(3)
+        # print(f"Amount: {amount}")
+        # print(f"Location: {location}")
+
+        regex2 = r"^(.*?)/(.*?)$"
+        match2 = re.search(regex2, amount)
+        amount_format = float(match2.group(1))
+
+        regex3 = r"^(.*?) \(.*?\)$"
+        location = location.strip()
+        match3 = re.search(regex3, location)
+        try:
+            location_format = match3.group(1)
+        except Exception as matchError: 
+            print(f"No match")
+        # print(f"Amount: {amount_format}")
+
+
+    else:
+        print("No match found")
+    return amount_format, location_format
+
+def cleanup():
+    labels = ["Carroll County", "Baltimore MD", "Gaithersburg", "Baltimore-Martin MD"]
+    csv_f = "D:/project/Data.csv"
+    image_dict = {}
+
+    with open("D:/project/data_new.json", "r") as f: 
+        data = json.load(f)
+    
+    print("Cleaning up...")
+        
+    with open(csv_f, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["CarrollCounty", "Baltimore", "Gaithersburg", "Baltimore-Martin","NoRain", "Image"])
+        counter = 0
+        for x in data: 
+            print(f"Counter: {counter}")
+            try:
+                if data[x]["picture"] != None:
+                    try: 
+                        # print(data[x]["precipitation"])
+                        # image_data = Image.open(os.path.join("D:/weather/", data[x]["picture"]))
+                        # with open(os.path.join("D:/weather/", data[x]["picture"]), 'rb') as image_file:
+                        #     image_data = image_file.read()
+                        # pixel_data = list(image.getdata()) 
+                        # serialized_data = json.dumps(pixel_data)
+                        # serialized_data = pickle.dumps(image_data)
+                        # image_dict[counter] = [base64.b64encode(serialized_data).decode('utf-8')]
+                        
+                        # No rain most likely if it didn't record any precipitation
+                        write_list = [0, 0, 0, 0, 0, data[x]["picture"]]
+                        source = os.path.join("D:/weather/", data[x]["picture"])
+                        destination = os.path.join("D:/weather_used/", data[x]["picture"])
+                        # if (data[x]["picture"]) not in image_dict:
+                        image_dict[data[x]["picture"]] = destination
+
+                        if "precipitation" not in data[x]:
+                            # writer.writerow(write_list)
+                            # counter += 1
+                            pass
+                        else: 
+                            shutil.copy(source, destination)
+                            for i in data[x]["precipitation"]:
+                                amount, location = parsetext(i)
+                                location = location.strip()
+                                # print(f"Location: {location} Amount: {amount}")
+
+                                if amount > 0: 
+                                    amount = int(1)
+                                else: 
+                                    amount = int(0)
+                                if location == labels[0]:
+                                    write_list[0] = amount
+                                elif location == labels[1]:
+                                    write_list[1] = amount
+                                elif location == labels[2]:
+                                    write_list[2] = amount
+                                elif location == labels[3]: 
+                                    write_list[3] = amount
+                                else:
+                                    print(f"Error unknown location: {location}")
+                            # print(write_list)
+                            counter += 1
+                            if write_list[0] == 0 and write_list[1] == 0 and write_list[2] == 0 and write_list[3] == 0:
+                                write_list[4] = 1
+                            writer.writerow(write_list)
+
+                        # time.sleep(3)
+ 
+                
+                    except Exception as e: 
+                        print(f"Exception: {e}")
+                        print(f"Data: {data[x]}")
+                        # time.sleep(5)
+                        pass
+            except Exception as enopic:
+                print(f"No picture available skipping.")
+
+    # with open("D:/project/Images.json", "w") as imageFile:
+    #     json.dump(image_dict, imageFile, indent = 4)
+        
+
+    print(f"Counter: {counter}")
+
+def parseTree(text):
+    regex = r"([0-9,]+[\.0-9]*)\s*\|\s*(.*?)\s*\("
+    match = re.search(regex, text)
+    if match: 
+        amount = match.group(1)
+        location = match.group(2)
+        # print(f"Amount: {amount}")
+        # print(f"Location: {location}")
+    return amount, location
+     
+
+def cleanupTree(): 
+    labels_precip = ["Carroll County", "Baltimore MD", "Gaithersburg", "Baltimore-Martin MD"]
+    # labels_pressure = ["FSNM2", "Baltimore MD", "Baltimore-Martin MD", "Gaithersburg", "TCBM2", "BUOY", "SHIP", "Patapsco", "BLTM2", "FSKM2"]
+    labels_pressure = ["FSNM2", "Baltimore MD", "Gaithersburg", "SHIP", "FSKM2", "BLTM2", "TCBM2", "Baltimore-Martin MD"]
+    # labels_dewpoint = ["Carroll County", "Baltimore-Martin MD", "Gaithersburg", "Baltimore MD"]
+    labels_dewpoint = ['Carroll County', 'Baltimore-Martin MD', 'Gaithersburg', 'Baltimore MD', 'SHIP'] 
+    # labels_dewpoint_spread = ["Gaithersburg","Baltimore MD","Carroll County","Baltimore-Martin MD"]
+    labels_dewpoint_spread = ['Gaithersburg', 'Baltimore MD', 'Carroll County', 'Baltimore-Martin MD', 'SHIP']
+    # labels_wetbulbtemp = ["Gaithersburg","Baltimore MD","Carroll County","Baltimore-Martin MD", "SHIP"]
+    labels_wetbulbtemp = ['Gaithersburg', 'Baltimore-Martin MD', 'Baltimore MD', 'SHIP']
+    # labels_temperature = ["FSKM2", "Baltimore MD", "Baltimore-Martin MD", "Gaithersburg", "TCBM2", "Patapsco, MD", "BUOY", "Carroll County"]
+    labels_temperature = ['Carroll County', 'FSKM2', 'Gaithersburg', 'TCBM2', 'Baltimore-Martin MD', 'Baltimore MD', 'FSNM2', 'BLTM2'] 
+    csv_f = "D:/project/Data_tree.csv"
+    json_labels = ["temperature", "dewpoint_spread", "dewpoint", "pressure", "wetbulbtemp"]
+    locations = {}
+
+    data_dict = {
+        "pressure": labels_pressure,
+        "dewpoint": labels_dewpoint,
+        "dps": labels_dewpoint_spread,
+        "wbt": labels_wetbulbtemp,
+        "temp":labels_temperature
+    }
+    
+    label = "temperature"
+    print(label)
+    
+
+    with open("D:/project/data_new_tree.json", "r") as f: 
+        data = json.load(f)
+
+
+    # Used to list data from the json file 
+    """
+    
+    for t in data: # t = time and date
+        for i in data[t]:
+            # if i != "precipitation" and i != "picture":
+            if i == label:
+                if data[t][i] != None: 
+                    for element in data[t][i]:
+                        # print(element)
+                        amount, location = parseTree(element)
+                        if location not in locations:
+                            locations[location] = 1
+                        else: 
+                            locations[location] += 1
+                        
+                            
+    """
+
+    # 30 elements
+    header = ['FSNM2_pressure', 'Baltimore MD_pressure', 'Gaithersburg_pressure', 'SHIP_pressure', 'FSKM2_pressure', 
+              'BLTM2_pressure', 'TCBM2_pressure', 'Baltimore-Martin MD_pressure', 'Carroll County_dewpoint', 'Baltimore-Martin MD_dewpoint', 
+              'Gaithersburg_dewpoint', 'Baltimore MD_dewpoint', 'SHIP_dewpoint', 'Gaithersburg_dps', 'Baltimore MD_dps',
+              'Carroll County_dps', 'Baltimore-Martin MD_dps', 'SHIP_dps', 'Gaithersburg_wbt', 'Baltimore-Martin MD_wbt', 
+              'Baltimore MD_wbt', 'SHIP_wbt', 'Carroll County_temp', 'FSKM2_temp', 'Gaithersburg_temp', 
+              'TCBM2_temp', 'Baltimore-Martin MD_temp', 'Baltimore MD_temp', 'FSNM2_temp', 'BLTM2_temp', "time"]
+    
+    print(f"Header len: {len(header)}")
+    with open(csv_f, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        # for x in data_dict:
+        #     for l in data_dict[x]:
+                # header.append(l + "_" + x)
+                # print(l)
+        
+        writer.writerow(header)
+        total_valid_data = 0
+        total_data = 0
+        for t in data: 
+            row = 31 * [None]
+            for l in data[t]:
+                if l in json_labels:
+                    for item in data[t][l]:
+                        amount, location = parseTree(item)
+                        amount = amount.replace(',','')
+                        # print(f"Amount: {amount}")
+                        # print(f"Location: {location}")
+
+                        #json_labels = ["temperature", "dewpoint_spread", "dewpoint", "pressure", "wetbulbtemp"]
+
+                        if l == json_labels[0]: 
+                            if location in data_dict["temp"]:
+                                hl = location + "_" + "temp"
+                                indx = header.index(hl)                                
+                                row[indx] = amount
+                        elif l == json_labels[1]:
+                            if location in data_dict["dps"]:
+                                hl = location + "_" + "dps"
+                                indx = header.index(hl)
+                                row[indx] = amount
+                        elif l == json_labels[2]:
+                            if location in data_dict["dewpoint"]:
+                                hl = location + "_" + "dewpoint"
+                                indx = header.index(hl)
+                                row[indx] = amount
+                        elif l == json_labels[3]:
+                            if location in data_dict["pressure"]:
+                                hl = location + "_" + "pressure"
+                                indx = header.index(hl)
+                                row[indx] = amount
+                        elif l == json_labels[4]:
+                            if location in data_dict["wbt"]:
+                                hl = location + "_" + "wbt" 
+                                indx = header.index(hl)
+                                row[indx] = amount
+
+                        else:
+                            print(f"Invalid Label: {l}")
+            # print(f"Time: {t}")
+            row_c = 0
+            for n in row:
+                if n == None:
+                    row_c += 1
+            if row_c >= 9: # total valid data = 9181 total data: 62189
+                pass
+            else:
+                # print(row) 
+                row[30] = t
+                writer.writerow(row)
+                total_valid_data +=1
+            total_data += 1
+        print(f"Total Data: {total_data}")
+        print(f"Total valid Data: {total_valid_data}")
+            
+            
+# K-Nearest Neighbor (KNN) with N = 5
+def imputate():
+    data = pd.read_csv('D:/project/Data_tree.csv')
+    # data = data.iloc[:, :-1]
+    dates = data.pop("time")
+    missing_columns = data.columns[data.isna().any()]
+    imputer = KNNImputer(n_neighbors=5)
+    data_imputed = imputer.fit_transform(data)
+    data_imputed_df = pd.DataFrame(data_imputed, columns=data.columns)
+    data = data_imputed_df
+    data.insert(len(data.columns), "time", dates)
+    data.to_csv('D:/project/Data_tree_imputed.csv', index=False)
+    print("Done")
+
+        
+    
+def Addprecipitation():
+    label_r = ["Carroll County", "Baltimore MD", "Gaithersburg", "Baltimore-Martin MD","NoRain", "Image"]
+    with open("D:/project/data_new.json", "r") as f: 
+        data = json.load(f)
+    file_path = "D:/project/Data_tree_imputed.csv"
+    count = 0
+    df = pd.read_csv(file_path)
+    precipitation = len(df) * [None]
+    print(len(df))
+    date_column = df.iloc[:, -1]
+    for d in date_column:
+        if d in data: 
+            row = [0, 0, 0, 0, 0]
+            if "precipitation" in data[d]:
+                row_index = df.index[df['time'] ==d].tolist()[0]
+                col_index = df.columns.get_loc("time")
+                count += 1
+                for place in data[d]["precipitation"]:
+                    amount, location = parsetext(place)
+                    # print(amount, location)
+                    location = location.strip()
+                    # print(f"Location: {location} Amount: {amount}")
+
+                    if amount > 0: 
+                        amount = int(1)
+                    else: 
+                        amount = int(0)
+                    if location == label_r[0]:
+                        row[0] = amount
+                    elif location == label_r[1]:
+                        row[1] = amount
+                    elif location == label_r[2]:
+                        row[2] = amount
+                    elif location == label_r[3]: 
+                        row[3] = amount
+                    else:
+                        print(f"Error unknown location: {location}")
+                # print(write_list)
+                if row[0] == 0 and row[1] == 0 and row[2] == 0 and row[3] == 0:
+                    row[4] = 1
+                
+                precipitation[row_index] = row
+                # print(row)
+
+    df.insert(len(df.columns), "precipitation", precipitation)
+    df_cleaned = df.dropna()
+    print(df_cleaned)
+    df_cleaned.to_csv('D:/project/Data_tree_full.csv', index=False)
+
+      
+             
+    print(f"Count: {count}")
+
+    
+    
+
+    
+    # Used to get the labels to use for the data
+    """
+    
+    total = 0
+    mean = 0
+    for x in locations: 
+        total += locations[x]
+        
+    
+    mean = (total / len(locations)) 
+    valid = []
+    print(f"Mean: {mean}")
+    for x in locations: 
+        if locations[x] >= mean: 
+            print(f"{x}: {locations[x]}")
+            valid.append(x)
+    print(valid)
+            
+
+    
+    
+    """
+    
+
+
+            
+            
+
+
+    
+
+
+
+
+
+
+if __name__ == '__main__':
+    # cleanup()
+    # cleanupTree()
+    # imputate()
+    # Addprecipitation()
+    pass
+    
---
+++ combine.py
@@ -0,0 +1,216 @@
+import base64
+from PIL import Image
+import cv2
+import json
+import re
+import datetime
+import os 
+import time
+
+
+
+# precip_data_f = open("data.json", "r")
+# precip_data = json.load(precip_data_f)
+# precip_data_f.close()
+
+# pressure_data_f = open("data_pressure.json", "r")
+# pressure_data = json.load(pressure_data_f)
+# pressure_data_f.close()
+
+# dewpoint_data_f = open("data_dewpoint.json", "r")
+# dewpoint_data = json.load(dewpoint_data_f)
+# dewpoint_data_f.close()
+
+dps_data_f = open("data_dpspread.json", "r")
+dps_data = json.load(dps_data_f)
+dps_data_f.close()
+
+humidity_data_f = open("data_humidity.json", "r")
+humidity_data = json.load(humidity_data_f)
+humidity_data_f.close()
+
+temp_data_file = open("data_temp.json", "r")
+temp_data = json.load(temp_data_file)
+temp_data_file.close()
+
+wb_data_file = open("data_wb.json", "r")
+wb_data = json.load(wb_data_file)
+wb_data_file.close()
+
+file = open("D:/project/data_new.json", 'r')
+master_data = json.load(file)
+file.close()
+
+def parseKey(key): 
+
+    regex = r"(\d{4})-(\d{2})-(\d{2})/(\d{2}):(\d{2})"
+    match = re.search(regex, key)
+    if match:
+        date = match.group(1) + "-" + match.group(2) + "-" + match.group(3)
+        time = match.group(4) + ":" + match.group(5)
+        # print(f"Date: {date}")
+        # print(f"Time: {time}")
+        date_time = date + " " + time
+        dt = datetime.datetime.strptime(date_time, "%Y-%m-%d %H:%M")
+    else:
+        print(f"Error: No match found: {key}")
+    
+    return(dt)
+    
+
+
+def parseFile(filename):
+    regex = r"(\d{4})-(\d{2})-(\d{2})\.(\d{2})_(\d{2})_(\d{2})\.png"
+    match = re.search(regex, filename)
+    if match:
+        year = match.group(1)
+        month = match.group(2)
+        day = match.group(3)
+        hour = match.group(4)
+        minute = match.group(5)
+        second = match.group(6)
+        # print("Year:", year)
+        # print("Month:", month)
+        # print("Day:", day)
+        # print("Hour:", hour)
+        # print("Minute:", minute)
+        # print("Second:", second)
+        date_time = f"{year}-{month}-{day} {hour}:{minute}:{second}" 
+        dt = datetime.datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S")
+        # print(dt)
+    else: 
+        print("Error: No match found: {filename}")
+    return dt
+
+if __name__ == "__main__": 
+    # master_data = {}
+    
+    # merge_item = 
+    # key_label = 
+
+
+    # merge_item = dewpoint_data
+    # key_label = "dewpoint"
+
+    # merge_item = dps_data
+    # key_label = "dewpoint_spread"
+
+    # merge_item = humidity_data
+    # key_label = "humidity"
+
+    # merge_item = temp_data
+    # key_label = "temperature"
+
+    
+    # merge_item = wb_data
+    # key_label = "wetbulbtemp"
+
+
+    # merge_item = pressure_data
+    # key_label = "pressure"
+
+    # merge_item = precip_data
+    # key_label = "precipitation"
+
+    def combineFiles(merge_item, key_label):
+        print("Combining...")
+        for key in merge_item: 
+            if key in master_data:
+                # print(f"Merge item key {merge_item[key]}")
+                master_data[key].update(merge_item[key])
+                # print(f"Merged: {master_data[key]}")
+            else: 
+                master_data[key] = merge_item[key]
+            
+        with open("D:/project/data_new.json", "w") as f: 
+            # master_data.update(precip_data)
+            json.dump(master_data, f, indent = 4)
+
+        print("Done!")
+    # Parse the keys into date and times 
+
+    # for key in master_data: 
+    #     parseKey(key)
+
+
+    # Image Combine
+
+    pictures = os.listdir("D:/weather")
+    # for picture in pictures: 
+        # ptime = parseFile(picture)
+        
+    possible_pictures = 0
+    better_pictures = 0
+    amazing_pictures = 0
+    count = 0
+    key_list = list(master_data.keys())
+    for key in key_list:
+        best_possible = None
+        best_possible_picture = None
+        print(f"Count: {count}/{len(key_list)}") 
+        if count % 100 == 0:
+            print(f"Total Same Day: {possible_pictures}") 
+            print(f"Total Same Hour: {better_pictures}") 
+            print(f"Total Within 30 min: {amazing_pictures}") 
+        k_time = parseKey(key)
+        count += 1
+        for picture in pictures:
+            ptime = parseFile(picture)
+            time_delta = abs(ptime - k_time)
+            # print(f"Difference: {time_delta.seconds}")
+            # time.sleep(1)
+            if(time_delta.days == 0):
+                # print(f"Same day: {time_delta.seconds}")
+                # print(f"Master Dict: {k_time}")
+                # print(f"Picture: {ptime}")
+                
+                # loaded_image = Image.open(os.path.join("D:/weather/", picture))
+                # width, height = loaded_image.size
+                
+                # if width == 760 and height == 616: 
+                if best_possible_picture == None or best_possible == None:
+                    best_possible_picture = picture
+                    best_possible = time_delta
+
+                elif(best_possible.seconds > time_delta.seconds):
+                    best_possible_picture = picture
+                    best_possible = time_delta
+
+                possible_pictures += 1
+                # else:
+                #     print(f"Bad image: {picture}")
+                    
+
+                # possible_pictures.append(time_delta)
+                if(time_delta.seconds <= 3600):
+                    # better_pictures.append(time_delta.seconds)
+                    better_pictures += 1
+                if(time_delta.seconds <= 1800):
+                    # amazing_pictures.append(time_delta.seconds)
+                    amazing_pictures += 1
+                    
+                # time.sleep(1)
+                 
+        
+        if "picture" not in master_data[key]:
+            # print("New Key")
+            master_data[key]["picture"] = best_possible_picture
+            if count % 1000 == 0: 
+                with open("D:/project/data_new.json", "w") as f: 
+                    json.dump(master_data, f, indent = 4) 
+        else:
+            print("Key already exists")
+            pass
+                
+    with open("D:/project/data_new.json", "w") as f: 
+        json.dump(master_data, f, indent = 4) 
+
+    
+    print(f"Total Same Day: {possible_pictures}") 
+    print(f"Total Same Hour: {better_pictures}") 
+    print(f"Total Within 30 min: {amazing_pictures}") 
+    
+    print("Done!")
+        
+        
+        
